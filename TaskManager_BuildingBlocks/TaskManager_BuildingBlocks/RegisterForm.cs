8137c807f686b0caaf81fe4fc59d1296dff47873
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-03-23 10:39:16 +0100

    Add database connection + Update user registration

--- TaskManager_BuildingBlocks/TaskManager_BuildingBlocks/RegisterForm.cs
+++ TaskManager_BuildingBlocks/TaskManager_BuildingBlocks/RegisterForm.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -25,17 +29,73 @@ namespace TaskManager_BuildingBlocks
 
         private void CreateUser()
         {
+            // generate a 128-bit salt using a cryptographically strong random sequence of nonzero values
+            byte[] salt = new byte[128 / 8];
+
             string email = emailTxb.Text;
-            string password = passTxb.Text;
+            string password = hash(passTxb.Text, salt);
             string rePass = rePassTxb.Text;
             bool isAdmin = isAdminCbx.Checked;
 
-            if (password != rePass) { MessageBox.Show("Passwords do not match"); return; }
-
+            if (password != rePass && !Regex.IsMatch(emailTxb.Text, @"^[a-z\.\-]+@[a-z0-9]+\.[a-z]{2,3}$")) 
+            { MessageBox.Show("Passwords do not match"); return; }
+            else { Register(salt, email, password, isAdmin); }
             /// TODO 
             ///     Make inputs red if not correct
             ///     check if email exist
             ///     Add user to DB
+        }
+
+        private void Register(byte[] salt, string email, string password, bool isAdmin)
+        {
+            DbConnection conn = new DbConnection();
+            string query = "INSERT INTO Users (Email, Password, Salt, IsAdmin) VALUES (@email, @pass, @salt, @admin)";
+            SqlCommand cmd;
+
+            try
+            {
+                int intSalt = BitConverter.ToInt32(salt, 0); //reset to byte[] => BitConverter.GetBytes;
+                byte admin = 0;
+
+                if (isAdmin) { admin = 1; }
+
+                DbConnection.OpenDbConnenction();
+                cmd = new SqlCommand(query, DbConnection.cnn);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@pass", password);
+                cmd.Parameters.AddWithValue("@salt", intSalt);
+                cmd.Parameters.AddWithValue("@admin", admin);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("User has been created.");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Inner Exception: " + ex.Message);
+                Console.WriteLine();
+                Console.WriteLine("Query Executed: " + query);
+                Console.WriteLine();
+                throw;
+            }
+        }
+
+        private string hash(string pass, byte[] salt)
+        {
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+
+            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: pass,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
         }
     }
 }
