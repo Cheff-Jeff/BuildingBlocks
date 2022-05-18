4bbc91ecaa9d443a2860f0092db75a9c83f1c09e
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-05-18 15:07:49 +0200

    update register form

--- TaskManager_BuildingBlocks/WebApp/Controllers/AccountController.cs
+++ TaskManager_BuildingBlocks/WebApp/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using DataLayer;
+using BusinessLayer;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -21,6 +24,38 @@ namespace WebApp.Controllers
                 return View();
             }
             return RedirectToAction("Index", "Account");
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                Login handler = new Login(new LoginDAL());
+                Login loginData = new Login(user.Email);
+                if (handler.LoginUserCheck(loginData))
+                {
+                    Login loginData1 = new Login(user.Email);
+                    handler.GetUserByEmail(loginData1);
+                    bool loginChecker = handler.VerifyPassword(user.Password, Login.usersalt, Login.userpassword);
+                    if (!loginChecker)
+                    {
+                        ViewBag.error = "You're password or email was incorrect.";
+                        return View();
+                    }
+                    else
+                    {
+                        HttpContext.Session.SetInt32("isAdmin", 1);
+                        return RedirectToAction("Privacy", "Home");
+                    }
+                }
+                else
+                {
+                    ViewBag.error = "There is no user with this email.";
+                    return View();
+                }
+            }
+            return View();
         }
     }
 }
