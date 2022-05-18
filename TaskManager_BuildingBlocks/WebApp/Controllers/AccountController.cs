b93480f8eea1789b1e48c4b2decbba47285d25d2
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-05-18 13:21:17 +0200

    update login

---
+++ TaskManager_BuildingBlocks/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32("isAdmin") != null)
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register() 
+        {
+            if (HttpContext.Session.GetInt32("isAdmin") != null && HttpContext.Session.GetInt32("isAdmin") == 1)
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Account");
+        }
+    }
+}
--- TaskManager_BuildingBlocks/WebApp/Controllers/HomeController.cs
+++ TaskManager_BuildingBlocks/WebApp/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Models;
+using BusinessLayer;
+using DataLayer;
+using Microsoft.AspNetCore.Http;
 
 namespace WebApp.Controllers
 {
@@ -29,8 +32,34 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel user)
+        public IActionResult Index(LoginViewModel user)
         {
+            if (ModelState.IsValid)
+            {
+                Login handler = new Login(new LoginDAL());
+                Login loginData = new Login(user.Email);
+                if (handler.LoginUserCheck(loginData))
+                {
+                    Login loginData1 = new Login(user.Email);
+                    handler.GetUserByEmail(loginData1);
+                    bool loginChecker = handler.VerifyPassword(user.Password, Login.usersalt, Login.userpassword);
+                    if (!loginChecker)
+                    {
+                        ViewBag.error = "You're password or email was incorrect.";
+                        return View();
+                    }
+                    else
+                    {
+                        HttpContext.Session.SetInt32("isAdmin", 1);
+                        return RedirectToAction("Privacy", "Home");
+                    }
+                }
+                else 
+                {
+                    ViewBag.error = "There is no user with this email.";
+                    return View();
+                }
+            }
             return View();        
         }
 
