063455ccdbfb30dab7834c39de20051519f95231
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-05-19 13:37:53 +0200

    add registratie

--- TaskManager_BuildingBlocks/WebApp/Controllers/AccountController.cs
+++ TaskManager_BuildingBlocks/WebApp/Controllers/AccountController.cs
@@ -12,6 +12,7 @@ namespace WebApp.Controllers
         {
             if (HttpContext.Session.GetInt32("isAdmin") != null)
             {
+                ViewBag.succes = TempData["seccesss"];
                 return View();
             }
             return RedirectToAction("Index", "Home");
@@ -31,27 +32,21 @@ namespace WebApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                Login handler = new Login(new LoginDAL());
-                Login loginData = new Login(user.Email);
-                if (handler.LoginUserCheck(loginData))
+                UserContainer userContainer = new UserContainer(new UserDAL());
+
+                byte[] salt = userContainer.AddSalt();
+                string Email = user.Email.ToLower();
+                var Password = userContainer.hash(user.Password, salt);
+
+                if (!userContainer.CheckUserExists(new User(user.Email.ToLower())))
                 {
-                    Login loginData1 = new Login(user.Email);
-                    handler.GetUserByEmail(loginData1);
-                    bool loginChecker = handler.VerifyPassword(user.Password, Login.usersalt, Login.userpassword);
-                    if (!loginChecker)
-                    {
-                        ViewBag.error = "You're password or email was incorrect.";
-                        return View();
-                    }
-                    else
-                    {
-                        HttpContext.Session.SetInt32("isAdmin", 1);
-                        return RedirectToAction("Privacy", "Home");
-                    }
+                    userContainer.UserRegister(new User(salt, user.Email, Password, user.IsAdmin));
+                    TempData["seccesss"] = "New user has been added.";
+                    return RedirectToAction("Index", "Account");
                 }
                 else
                 {
-                    ViewBag.error = "There is no user with this email.";
+                    ViewBag.error = "There already exists a user with this email.";
                     return View();
                 }
             }
