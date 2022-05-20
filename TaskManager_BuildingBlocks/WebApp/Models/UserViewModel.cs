10f54078653939419e5ae75dac8386e46c95ad96
Mattijs Pronk <456410@student.fontys.nl>
2022-05-20 12:46:15 +0200

    Usermanagement view added

---
+++ TaskManager_BuildingBlocks/WebApp/Models/UserViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class UserViewModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        public string HiddenPassword = "******";
+
+        [Required]
+        public bool IsAdmin { get; set; }
+
+
+
+        public List<UserViewModel> listusers = new List<UserViewModel>();
+
+        public UserViewModel()
+        {
+
+        }
+
+        public UserViewModel(int id, string email, bool isadmin)
+        {
+            this.UserId = id;
+            this.Email = email;
+            this.IsAdmin = isadmin;
+        }
+    }
+}
