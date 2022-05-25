2bd1e93f9cff18aecc6cf6b9203e542750c7f161
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-05-25 11:21:10 +0200

    add register js validation

---
+++ TaskManager_BuildingBlocks/WebApp/wwwroot/js/register.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function () {
+    $("#emailInput").change(() => {
+        const email = $("#emailInput").val();
+        checkEmail(email);
+    });
+
+    $("#passwordInput").change(() => {
+        const password = $("#passwordInput").val();
+        checkPass(password);
+    });
+
+    $("#btnSubmit").click((e) => {
+        let emailValit = false;
+        let passValit = false;
+
+        if (checkEmail($("#emailInput").val())) {
+            emailValit = true;
+        }
+
+        if (checkPass($("#passwordInput").val())) {
+            passValit = true;
+        }
+
+        if (!emailValit || !passValit) {
+            event.preventDefault();
+        }
+    });
+
+    const checkEmail = (email) => {
+        if (email == "" || email == " " || email == null) {
+            $("#emailError").text("This input is required.");
+            return false;
+        }
+        else if (!/^[a-z0-9\.\-]+@[a-z0-9]+\.[a-z]{2,3}$/.test(email)) {
+            $("#emailError").text("Please enter a valid email.");
+            return false;
+        }
+        else {
+            $("#emailError").text("");
+            return true;
+        }
+    }
+
+    const checkPass = (password) => {
+        if (password == "" || password == " " || password == null) {
+            $("#passwordError").text("This input is required.");
+            return false;
+        }
+        else if (password.length < 1 || password.length > 100) {
+            console.log(password.length);
+            $("#passwordError").text("Your password must be between 1 and 100 characters long.");
+            return false;
+        }
+        else {
+            $("#passError").text("");
+            return true;
+        }
+    }
+});
